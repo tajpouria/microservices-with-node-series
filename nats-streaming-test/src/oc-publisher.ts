194c9705cc942f0c617aad891e3f88911c3eb052
tajpouria <tajpouria.dev@gmail.com>
2020-07-22 02:28:49 +0430

    Added -- ex-nats test script

---
+++ nats-streaming-test/src/oc-publisher.ts
@@ -0,0 +1,42 @@
+import nats from "node-nats-streaming";
+import { OrderCreatedPublisher, OrderStatus } from "@tajpouria/stub-common";
+import { randomBytes } from "crypto";
+
+console.clear();
+
+const sc = nats.connect("stub", "abc", {
+  url: "http://localhost:4222",
+});
+
+sc.on("connect", async () => {
+  log("Connected");
+
+  const publisher = new OrderCreatedPublisher(sc);
+
+  await publisher.publish({
+    id: Math.random().toString(),
+    expiresAt: new Date().toISOString(),
+    status: OrderStatus.Created,
+    ticket: {
+      userId: Math.random().toString(),
+      id: Math.random().toString(),
+      title: randomBytes(4).toString("hex"),
+      price: 200.1,
+      timestamp: Date.now(),
+    },
+    userId: Math.random().toString(),
+    version: 1,
+  });
+
+  sc.on("close", () => {
+    log("Connection closed");
+    process.exit(1);
+  });
+});
+
+function log(msg: string) {
+  console.info(`Publisher: ${msg}`);
+}
+
+process.on("SIGINT", () => sc.close());
+process.on("SIGTERM", () => sc.close());
